5ef89d2b5d411b050584afd9e70686e7eab55209
Duy-Anh Dang <114787652+MtnDoob@users.noreply.github.com>
2024-04-17 23:37:11 -0700

    Update main.py with encoder
    encoder and distance

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 # TODO organize inputs here. 
-
+import forward from methods.py
 
 
 """
 This should be where all control @pathfinding will focus on coding. class togo_bot will have all abstractions. 
 """
 def main(): 
-    pass
+
+    '''
+    1/64 encoder 
+    2 * pi ~ 6 inches
+
+    init 300 -> 620 // difference is 320
+    320/64 = 5 rotations
+    5 * 6 inches = 30 inches
+    '''
+    turnRight90()
+    forward()
 
 
 if __name__ == "__main__": 
